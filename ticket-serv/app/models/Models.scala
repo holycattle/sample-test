e78a56b2e7bf8264116bb2104e2facb8a005897d
Joniel Ibasco <joniel@altitude-games.com>
2015-08-20 02:52:07 +0800

    Implement a DAO for Users

--- ticket-serv/app/models/Models.scala
+++ ticket-serv/app/models/Models.scala
@@ -1,7 +1,13 @@
 package models
 
+import javax.inject.{Singleton, Inject}
+import scala.concurrent.Future
+
 import play.api._
 import play.api.libs.json.Json
+
+import play.db.NamedDatabase
+import play.api.db.slick._
 import slick.driver.JdbcProfile
 
 case class User(id: Long, name: String, password: String, email: String, groupId: Int)
@@ -14,7 +20,7 @@ trait UserTable {
   protected val driver: JdbcProfile
   import driver.api._
   
-  class Users(tag: Tag) extends Table[User](tag, "users") {
+  class UserModel(tag: Tag) extends Table[User](tag, "users") {
     def id = column[Long]("id", O.PrimaryKey)
     def name = column[String]("name", O.PrimaryKey)
     def password = column[String]("password")
@@ -22,5 +28,25 @@ trait UserTable {
     def groupId = column[Int]("group_id")
 
     def * = (id, name, password, email, groupId) <> ((User.apply _).tupled, User.unapply _)
+  }
+}
+
+@Singleton()
+class Users @Inject() (@NamedDatabase("vagrant") protected val dbConfigProvider: DatabaseConfigProvider) extends UserTable with HasDatabaseConfig[JdbcProfile] {
+  val dbConfig = dbConfigProvider.get[JdbcProfile]  
+  import driver.api._
+
+  val users = TableQuery[UserModel]
+
+  def all(): Future[Seq[User]] = {
+    db.run(users.result)
+  }
+
+  def getByEmailAndPassword(email: String, password: String): Future[Option[User]] = {
+    val query = for {
+      u <- users if u.email === email && u.password === password
+    } yield u
+    db.run(users.filter(_.email === email).result.headOption)
+    //db.run(query)
   }
 }
